964f96012e2f96c989e9434398387064dfce5208
AKAviles <avilesanthony0617@gmail.com>
2023-12-13 11:26:11 -0600

    feat: added initial rendering framework

--- src/components/level-layout/RenderLevel.js
+++ src/components/level-layout/RenderLevel.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import styles from './RenderLevel.module.css'
 import Sprite from '../object-graphics/Sprite'
+import { CELL_SIZE } from '@/helpers/consts'
 
 export default function RenderLevel({spriteSheetImage}) {
+
+  const level = {
+    placements: [
+      { id: 0, x: 0, y: 0, frameCoord: "0x2"},
+      { id: 1, x: 1, y: 1, frameCoord: "0x2"},
+      { id: 2, x: 2, y: 2, frameCoord: "0x2"},
+      { id: 3, x: 3, y: 3, frameCoord: "0x2"},
+      { id: 4, x: 4, y: 4, frameCoord: "0x2"},
+      { id: 5, x: 5, y: 5, frameCoord: "0x2"},
+      { id: 6, x: 6, y: 6, frameCoord: "0x2"}
+    ]
+  }
   return (
     <div className={styles.fullScreenContainer}>
         <div className={styles.gameScreen}>
-            <Sprite image={spriteSheetImage} frameCoord={"6x6"} />
-            <Sprite image={spriteSheetImage} frameCoord={"1x9"} />
-            <Sprite image={spriteSheetImage} frameCoord={"0x9"} />
+          {level.placements.map(placement => {
+            //Wrap each sprite in a positioned div
+            const x = placement.x * CELL_SIZE + "px";
+            const y = placement.y * CELL_SIZE + "px";
+
+            const style = {
+              position: 'absolute',
+              transform: `translate3d(${x}, ${y}, 0)`
+            }
+
+            return (
+              <div key={placement.id} style={style}>
+                <Sprite image={spriteSheetImage} frameCoord={placement.frameCoord} />
+              </div>
+            )
+          })}
         </div>
     </div>
   )
